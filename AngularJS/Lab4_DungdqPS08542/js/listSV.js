fd9624a0feaf41a36544c2254934c1a3dabed3c0
DungRoyal <0398022720 - Doanquocdung55@gmai.com>
2020-07-21 00:45:34 +0700

    Done Lab4

---
+++ AngularJS/Lab4_DungdqPS08542/js/listSV.js
@@ -0,0 +1,31 @@
+var listsv = [{
+        "username": "dungdq",
+        "password": "123",
+        "fullname": "Đoàn Quốc Dũng",
+        "email": "dungdqps08542@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "05/05/2000",
+        "schoolfee": "6500000",
+        "marks": "9.0"
+    },
+    {
+        "username": "pheonv",
+        "password": "iloveyou",
+        "fullname": "Nguyễn Văn Chí Phèo",
+        "email": "pheonv@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "1985-10-11",
+        "schoolfee": "2500000",
+        "marks": "0"
+    },
+    {
+        "username": "nopt",
+        "password": "iloveyou",
+        "fullname": "Phạm Thị Nở",
+        "email": "nopt@fpt.edu.vn",
+        "gender": "false",
+        "birthday": "1993-05-15",
+        "schoolfee": "2000000",
+        "marks": "0"
+    }
+]
