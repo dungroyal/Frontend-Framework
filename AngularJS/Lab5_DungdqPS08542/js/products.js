c3166c04b4da307f6a952dae2ccf80b1dd86117c
DungRoyal <0398022720 - Doanquocdung55@gmai.com>
2020-07-25 00:18:35 +0700

    99%

---
+++ AngularJS/Lab5_DungdqPS08542/js/products.js
@@ -0,0 +1,25 @@
+var products = [{
+    name: "Samsung Galaxy Tab A",
+    image: "1.png",
+    price: 1000
+}, {
+    name: "Máy tính bảng Mobell",
+    image: "2.png",
+    price: 1500
+}, {
+    name: "Điện thoại Iphone 8",
+    image: "3.png",
+    price: 2000
+}, {
+    name: "Điện thoại BlackBarry",
+    image: "4.png",
+    price: 3000
+}, {
+    name: "Điện thoại Realme 5",
+    image: "5.png",
+    price: 2000
+}, {
+    name: "Điện thoại Nokia 3.2",
+    image: "6.png",
+    price: 1700
+}]
